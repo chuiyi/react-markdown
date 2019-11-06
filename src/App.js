f458843a60ccce782de050b110a9ccdb60b6a70e
Chuiyi Lin <chuiyilin@gmail.com>
2019-11-07 03:27:40 +0800

    markdown editor ui

--- src/App.js
+++ src/App.js
@@ -1,30 +1,80 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import InputGroup from 'react-bootstrap/InputGroup'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 import ReactMarkdown from 'react-markdown'
-const input = '# This is a header\n\nAnd this is a paragraph'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <ReactMarkdown source={input} />
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      editor: {
+        subject: '',
+        content: ''
+      }
+    }
+  }
+  editorContentChanged = (e) => {
+    this.setState({
+      editor: {
+        content: e.target.value
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#">React Markdown Editor</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        </Navbar>
+        <Container fluid>
+          <Row>
+            <Col xs={2} className="area-list-background">
+              <Container className="area-list">
+                1 of 3
+            </Container></Col>
+            <Col xs={5} className="area-editor-background">
+              <Container className="area-editor">
+                <div>
+                  <InputGroup className="mb-3">
+                    <FormControl
+                      placeholder="Subject"
+                      aria-label="Subject"
+                      aria-describedby="basic-addon2"
+                    />
+                    <InputGroup.Append>
+                      <Button variant="outline-secondary">儲存</Button>
+                    </InputGroup.Append>
+                  </InputGroup>
+                  <FormControl
+                    className="area-editor-content"
+                    as="textarea"
+                    aria-label="With textarea"
+                    onChange={this.editorContentChanged} />
+                </div>
+              </Container>
+            </Col>
+            <Col xs={5}>
+              <Container className="area-markdown">
+                <ReactMarkdown source={this.state.editor.content} />
+              </Container>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    )
+  }
 }
 
 export default App;
